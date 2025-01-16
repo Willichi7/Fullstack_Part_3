db9e70879aadbf322252a2d804c86e5def55ed37
LiamWill <161132622+Willichi7@users.noreply.github.com>
2025-01-16 14:39:39 +0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const morgan = require('morgan')
 const date = Date('en-US')
 
 app.use(express.json())
-app.use(express.static('dist'))
+app.use(express.static(path.join(__dirname, 'frontend/dist')));
 const persons = [
    { 
      "id": "1",
