d5f9fd6178ed87c4c134d8379de0868814be4ce9
LiamWIll <willywillsken@gmail.com>
2025-01-20 20:23:47 +0400

    deploying

--- backend/models/person.js
+++ backend/models/person.js
@@ -9,35 +9,34 @@ mongoose.set('strictQuery', false)
 console.log('Connecting to:', url)
 
 // connect the database
-mongoose.connect(url)
-   .then((_) => {
-   console.log('Connected to MongoDB')
+mongoose.connect(url).then((_) => {
+  console.log('Connected to MongoDB')
 }).catch(error => {
-   console.log('Error connecting to MongoDB:', error)
+  console.log('Error connecting to MongoDB:', error)
 })
 
 // database Schema
 const personSchema = new mongoose.Schema({
-   name: { type: String,  required: true },
-   number: {
-      type: String,
-      minLength: 8,
-      required: [true, 'User Phone number is required'],
-      validate: {
-         validator: (v) => {
-            return /^\d{2,3}-\d+$/.test(v);
-         },
-         message: props => `${props.value} is not a valid phone number!`
-      }
-   },
+  name: { type: String,  required: true },
+  number: {
+    type: String,
+    minLength: 8,
+    required: [true, 'User Phone number is required'],
+    validate: {
+      validator: (v) => {
+        return /^\d{2,3}-\d+$/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    }
+  },
 })
 
 personSchema.set('toJSON', {
-   transform: (_, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject._v
-   }
+  transform: (_, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject._v
+  }
 })
 
 // database model
